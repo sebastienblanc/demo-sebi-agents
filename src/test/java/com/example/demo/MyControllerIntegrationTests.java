7fb6c293f6e08ae83556592dabe9855f4c9da5bc
Sébastien Blanc <sebastienblanc@MacBook-Pro-de-Sebastien.local>
2026-02-09 08:57:56 +0100

    test: update AutoConfigureWebTestClient import to Spring Boot 4 package (fixes #19)

--- src/test/java/com/example/demo/MyControllerIntegrationTests.java
+++ src/test/java/com/example/demo/MyControllerIntegrationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
